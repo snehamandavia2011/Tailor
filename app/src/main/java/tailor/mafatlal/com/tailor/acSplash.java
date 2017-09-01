656ad544cb764e679e6c42bac90fd719f203ac22
Sneha Mandavia <sm@360io.co>
2017-09-01 15:10:52 +0530

    Initial commit

---
+++ app/src/main/java/tailor/mafatlal/com/tailor/acSplash.java
@@ -0,0 +1,17 @@
+package tailor.mafatlal.com.tailor;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import com.crashlytics.android.Crashlytics;
+import io.fabric.sdk.android.Fabric;
+
+
+public class acSplash extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        Fabric.with(this, new Crashlytics());
+        setContentView(R.layout.ac_splash);
+    }
+}
